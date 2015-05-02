2282653593065e80abfefa92f42c5f308c460378
Greg Bowler <greg.bowler@g105b.com>
2015-05-02 17:23:06 +0100

    Output year view to index

--- src/Page/index.php
+++ src/Page/index.php
@@ -6,6 +6,14 @@ use Esendex\InboxService;
 class Index extends \Gt\Page\Logic {
 
 public function go() {
+	for($year = 1990; $year <= 2015; $year++) {
+		$li = $this->template->get("year-block");
+		$li->querySelector("time")->textContent = $year;
+		$li->insertTemplate();
+	}
+}
+
+private function testSMS() {
     $authentication = new LoginAuthentication(
         "EX0159513",
         "cinematimemachine@g105b.com",
