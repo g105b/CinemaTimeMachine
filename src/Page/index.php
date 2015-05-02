5a5a4ed0d4b66752456b096390a7ff77729bdef4
Greg Bowler <greg.bowler@g105b.com>
2015-05-02 20:00:01 +0100

    Output dummy images

--- src/Page/index.php
+++ src/Page/index.php
@@ -9,6 +9,12 @@ public function go() {
 	for($year = 1990; $year <= 2014; $year++) {
 		$li = $this->template->get("year-block");
 		$li->querySelector("time")->textContent = $year;
+
+		foreach($li->querySelectorAll("img") as $img) {
+			// Randomise image URL to get different images back.
+			$img->src .= "?rand=" . mt_rand(1000,9999);
+		}
+
 		$li->insertTemplate();
 	}
 }
