4674c67700f52c1a13d32447ae70ddb0270605d4
Greg Bowler <greg.bowler@g105b.com>
2015-05-02 18:03:14 +0100

    Remove esendex test

--- src/Page/index.php
+++ src/Page/index.php
@@ -6,28 +6,11 @@ use Esendex\InboxService;
 class Index extends \Gt\Page\Logic {
 
 public function go() {
-	for($year = 1990; $year <= 2015; $year++) {
+	for($year = 1990; $year <= 2014; $year++) {
 		$li = $this->template->get("year-block");
 		$li->querySelector("time")->textContent = $year;
 		$li->insertTemplate();
 	}
-}
-
-private function testSMS() {
-    $authentication = new LoginAuthentication(
-        "EX0159513",
-        "cinematimemachine@g105b.com",
-        base64_decode("c3VwZXJzZWN1cmVwbGVhc2Vkb25vdHN0ZWFsdGhpcw==")
-    );
-
-    $service = new InboxService($authentication);
-    $messageList = $service->latest();
-
-    foreach($messageList as $message) {
-        echo "<p>" . $message->summary();
-    }
-
-    die();
 }
 
 }#
