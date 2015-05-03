045079111940d439b3f52f3f6ddbbbd48999754f
Greg Bowler <greg.bowler@g105b.com>
2015-05-03 08:24:10 +0100

    Add unschedule capability for when SMS is received (#2)

--- src/Script/Namespace/prompt.js
+++ src/Script/Namespace/prompt.js
@@ -3,16 +3,22 @@ ns("CTM.Prompt", (function() {
 var
 	delaySeconds = 10,
 	promptShowSeconds = 5,
+	timeout,
 $$;
 
 function schedule() {
 	CTM.debug && console.log("Scheduling prompt...");
-	setTimeout(promptTimeout, delaySeconds * 1000);
+	timeout = setTimeout(promptTimeout, delaySeconds * 1000);
+}
+
+function unschedule() {
+	clearTimeout(timeout);
 }
 
 function promptTimeout() {
 	CTM.debug && console.log("Prompt timeout.");
 	x.updatePage("/prompt/example1", updatePageCallback);
+	timeout = null;
 }
 
 function updatePageCallback() {
@@ -21,13 +27,14 @@ function updatePageCallback() {
 	setTimeout(removePrompt, promptShowSeconds * 1000);
 }
 
-function removePrompt() {
+	function removePrompt() {
 	CTM.debug && console.log("Removing prompt.");
 	document.querySelector("main.overlay").remove();
 }
 
 return {
 	schedule: schedule,
+	unschedule: unschedule,
 };
 
 })());
