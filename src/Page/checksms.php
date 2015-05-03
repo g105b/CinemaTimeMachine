c1a6d3241d4000caed48d7da45afb1d4c7450b29
Greg Bowler <greg.bowler@g105b.com>
2015-05-03 08:50:23 +0100

    Set timestamp on server

--- src/Page/checksms.php
+++ src/Page/checksms.php
@@ -8,9 +8,31 @@ use CTM\Esendex\Inbox;
 class CheckSms extends \Gt\Page\Logic {
 
 public function go() {
+	if(isset($_GET["timestamp"])) {
+		$this->session->delete("SmsTime");
+		$this->session->set("SmsTime", $_GET["timestamp"]);
+		die("timestamp is now $_GET[timestamp]");
+	}
+
     $esendex = new Inbox();
     $messageArray = $esendex->getList();
-    var_dump($messageArray);die();
+
+	// The minimum timestamp to return:
+    $smsTime = 0;
+
+    if($this->session->exists("SmsTime")) {
+		$smsTime = $this->session->get("SmsTime");
+	}
+
+	foreach($messageArray as $timestamp => $message) {
+		if($timestamp <= $smsTime) {
+			unset($messageArray[$timestamp]);
+		}
+	}
+
+	$messageArray = array_reverse($messageArray);
+
+	die(json_encode($messageArray));
 }
 
 }#
--- src/Script/Namespace/SMS.js
+++ src/Script/Namespace/SMS.js
@@ -15,6 +15,25 @@ function checkInterval() {
 
 function checkSmsCallback() {
 	CTM.debug && console.log("SMS response: ", this.responseText);
+
+	var
+		smsArray = JSON.parse(this.responseText),
+		timestamp,
+	$$;
+
+	for(timestamp in smsArray) {
+		showSms(smsArray[timestamp]);
+		markAsRead(timestamp);
+	}
+}
+
+/**
+ * Sets the session on the server as to not show any new messages.
+ *
+ * @param timestamp
+ */
+function markAsRead(timestamp) {
+	x.get("/checksms?timestamp=" + timestamp);
 }
 
 return {
